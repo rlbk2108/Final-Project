c7d66dac5413f6b0f4d4d945e865815d380a2867
Eraaly Kasymbekov <sanazera2@gmail.com>
2021-12-02 00:04:07 +0600

    second commit

--- venvv/Warehouse/apps/ware/models.py
+++ venvv/Warehouse/apps/ware/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-from core.models import Product
 from django.contrib.auth.models import User
 from datetime import datetime
+from django.conf import settings
+from django.db.models.signals import post_save
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 CATEGORY = (
     ('print', 'Принтер'),
@@ -9,19 +13,58 @@ CATEGORY = (
     ('laptop', 'Ноутбук'),
 )
 
-class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=datetime.now)
-
-
-class CartProduct(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+class Product(models.Model):
     product_name = models.CharField('Название товара', max_length=100)
     category = models.CharField('Категория', max_length=10, default='print', choices=CATEGORY)
     product_number = models.IntegerField('Количество товара', default=0)
-    date_pub = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.product_name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ebooks = models.ManyToManyField(Product, blank=True)
+    stripe_id = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.user
+
+
+def post_save_profile_create(sender, instance, created, *args, **kwargs):
+    user_profile, created = Profile.objects.get_or_create(user=instance)
+
+    if user_profile.stripe_id is None or user_profile.stripe_id == '':
+        new_stripe_id = stripe.Customer.create(email=instance.email)
+        user_profile.stripe_id = new_stripe_id['id']
+        user_profile.save()
+
+
+post_save.connect(post_save_profile_create, sender=settings.AUTH_USER_MODEL)
+
+class OrderItem(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.SET_NULL, null=True)
+    is_ordered = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now=True)
+    date_ordered = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.product
+
+
+class Order(models.Model):
+    ref_code = models.CharField(max_length=15)
+    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    is_ordered = models.BooleanField(default=False)
+    items = models.ManyToManyField(OrderItem)
+    date_ordered = models.DateTimeField(auto_now=True)
+
+    def get_cart_items(self):
+        return self.items.all()
+
+    def get_cart_total(self):
+        return sum([item.product.price for item in self.items.all()])
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.owner, self.ref_code)
 
